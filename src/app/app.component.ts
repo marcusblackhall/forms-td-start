5c5cf26f1b32d269f1a26e90f0c9fc922909c1fd
Marcus Blackhall <marcusinamsterdam@gmail.com>
2022-07-06 11:07:26 +0200

    Setting and patching form values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,36 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
 
-defaultQuestion:string = "pet";
-answer: string = "";
-genders:string[] = ["male","female"];
+  defaultQuestion: string = "pet";
+  answer: string = "";
+  genders: string[] = ["male", "female"];
 
-@ViewChild('f') signupForm:NgForm
+  @ViewChild('f') signupForm: NgForm
 
   suggestUserName() {
+    console.log("assigning default values");
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue(
+    //   {
+    //     userData: {
+    //       username: 'Marcus',
+    //       email: 'something@google.nl'
+    //     },
+    //     secret : 'pet',
+    //     questionAnswer: '',
+    //     gender: 'male'
+
+    //   }
+
+    // );
+    this.signupForm.form.patchValue(
+        {
+          userData: {
+            username: "Marcus"
+          }
+        }
+
+    );
   }
 
   // onSubmit(form: NgForm){
@@ -23,7 +45,7 @@ genders:string[] = ["male","female"];
   // console.log("email is " + form.value.email);
   // }
 
-  onSubmit(){
-console.log(this.signupForm);
+  onSubmit() {
+    console.log(this.signupForm);
   }
 }
